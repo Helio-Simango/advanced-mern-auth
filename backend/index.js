c3a528153abc28b33904145a36bc1ba59d0cad1a
Helio-Simango <heliojanuariosimango@gmail.com>
2025-09-26 12:15:27 +0200

    fix: fixing bug with path-to-regexp, in a index.js file #2

--- backend/index.js
+++ backend/index.js
@@ -22,16 +22,12 @@ app.use(cors({
     credentials: true
 }));
 
-app.get("/", (req, res) =>{
-    res.send("Hello world")
-});
-
 app.use("/api/auth",authRoutes)
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("/*", (req, res) =>{
+    app.get("/.*/", (req, res) =>{
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
